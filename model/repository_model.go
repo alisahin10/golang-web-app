6a58d95249cf8840cb1740bb5aa26fb6a7a6382f
eraycan.uyanik <eraycan.uyanik@rapsodo.com>
2024-09-30 11:40:04 +0300

    repo functions added.

--- model/repository_model.go
+++ model/repository_model.go
@@ -1,4 +1,11 @@
 package model
 
 type User struct {
+	ID       string `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+	Name     string `json:"name"`
+	Lastname string `json:"lastname"`
+	Age      int    `json:"age"`
 }
--- repository/local/bunt.go
+++ repository/local/bunt.go
@@ -1,36 +1,125 @@
 package local
 
-import "gitlab.com/rapsodoinc/tr/architecture/golang-web-app/model"
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/tidwall/buntdb"
+	"gitlab.com/rapsodoinc/tr/architecture/golang-web-app/model"
+	"os"
+)
 
 type buntImpl struct {
+	db *buntdb.DB
 }
 
-func NewBuntRepository(dbPAth string) (Repository, error) {
-	// Create bunt db
+func NewBuntRepository(dbPath string) (Repository, error) {
+	db, err := buntdb.Open(dbPath)
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+
+	localDbPath := os.Getenv("LOCAL_DB_PATH")
+	if _, err = os.Stat(localDbPath); os.IsNotExist(err) {
+		err := os.MkdirAll(localDbPath, os.ModePerm)
+		if err != nil {
+			return nil, err
+		}
+	}
+
 	return &buntImpl{}, nil
 }
 
 func (repo *buntImpl) Create(user *model.User) error {
-	//TODO implement me
-	panic("implement me")
+	return repo.db.Update(func(tx *buntdb.Tx) error {
+		userJSON, err := json.Marshal(user)
+		if err != nil {
+			return err
+		}
+		err = repo.db.Update(func(tx *buntdb.Tx) error {
+			_, _, err := tx.Set(fmt.Sprintf("user:%s", user.ID), string(userJSON), nil)
+			return err
+		})
+
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+
 }
 
 func (repo *buntImpl) FindOneByID(userID string) (*model.User, error) {
-	//TODO implement me
-	panic("implement me")
+	var user model.User
+
+	// Read the user data from BuntDB
+	err := repo.db.View(func(tx *buntdb.Tx) error {
+		userJSON, err := tx.Get(fmt.Sprintf("user:%s", userID))
+		if err != nil {
+			return err
+		}
+
+		// Unmarshal JSON string back into User struct
+		err = json.Unmarshal([]byte(userJSON), &user)
+		return err
+	})
+
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
 }
 
 func (repo *buntImpl) FindAll() ([]*model.User, error) {
-	//TODO implement me
-	panic("implement me")
+	var users []*model.User
+
+	// Iterate through all users in the database
+	err := repo.db.View(func(tx *buntdb.Tx) error {
+		err := tx.Ascend("", func(key, value string) bool {
+			if len(key) > 5 && key[:5] == "user:" { // Check for keys with "user:" prefix
+				var user model.User
+				if err := json.Unmarshal([]byte(value), &user); err == nil {
+					users = append(users, &user)
+				}
+			}
+			return true // continue iteration
+		})
+		return err
+	})
+
+	if err != nil {
+		return nil, err
+	}
+	return users, nil
 }
 
 func (repo *buntImpl) UpdateOneByID(userID, email, name, lastname string, age int) error {
-	//TODO implement me
-	panic("implement me")
+	// Fetch the existing user
+	user, err := repo.FindOneByID(userID)
+	if err != nil {
+		return err
+	}
+	user.Email = email
+	user.Name = name
+	user.Lastname = lastname
+	user.Age = age
+	userJSON, err := json.Marshal(user)
+	if err != nil {
+		return err
+	}
+	err = repo.db.Update(func(tx *buntdb.Tx) error {
+		_, _, err := tx.Set(fmt.Sprintf("user:%s", userID), string(userJSON), nil)
+		return err
+	})
+	return err
 }
 
 func (repo *buntImpl) DeleteOneByID(userID string) error {
-	//TODO implement me
-	panic("implement me")
+	// Delete the user by ID
+	err := repo.db.Update(func(tx *buntdb.Tx) error {
+		_, err := tx.Delete(fmt.Sprintf("user:%s", userID))
+		return err
+	})
+
+	return err
 }
